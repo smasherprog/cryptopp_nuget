0e1bf89dd25cd86c81f363037892b6ae6cd99028
smasherprog <smasherprog@gmail.com>
2016-04-02 17:55:26 -0700

    Removed file

--- cleanbuild.bat
+++
@@ -1,9 +0,0 @@
-for /r . %%g in (*.nupkg) do del %%g 	
-call del %1.tar
-call del pax_global_header
-call rd /s /q %1
-call rd /s /q "libjpeg-turbo-src-32-VS2013"
-call rd /s /q "libjpeg-turbo-src-64-VS2013"
-call rd /s /q "libjpeg-turbo-src-32-VS2015"
-call rd /s /q "libjpeg-turbo-src-64-VS2015"
-exit
